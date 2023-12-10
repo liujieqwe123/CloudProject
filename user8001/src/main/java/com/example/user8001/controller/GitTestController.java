c9a30724d70759fc95b1eae2e363182b144ea437
pony <492725168@qq.com>
2023-12-10 12:22:06 +0800

    test reset hard2

--- user8001/src/main/java/com/example/user8001/controller/GitTestController.java
+++ user8001/src/main/java/com/example/user8001/controller/GitTestController.java
@@ -2,6 +2,7 @@ package com.example.user8001.controller;
 
 public class GitTestController {
     public static void main(String[] args) {
+        System.out.println("fdkjdsfjsdjsd");
         System.out.println("fkjsdjfdskjds");
     }
 }
